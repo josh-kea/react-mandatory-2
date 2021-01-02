06a649d9d0d9dee079763bd6eaf3ca16f63ce523
Joshua Kaplan <joshkap2015@gmail.com>
2021-01-02 21:44:16 +0100

    Adds basic login form working with setState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [state, setState] = useState( {
+    username: '',
+    password: ''
+  })
+
+  const username = state.username
+  const password = state.password
+
+  function handleChange(stateProperty) {
+    return function(event) {
+      setState({ ...state, [stateProperty]: event.target.value })
+    };
+};
+
+    const handleSubmit = event => {
+      event.preventDefault();
+      console.table({ username, password })
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input type="text" value={username} name="username" onChange={handleChange('username')}></input>
+
+          <label>Password</label>
+          <input type="password" value={password} onChange={handleChange('password')} name="password"></input>
+
+          <button>Sign In</button>
+
+        </form>
     </div>
   );
 }
